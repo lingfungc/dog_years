74a877b11dfee8828a9ebf6c36b799e90c71e4a1
Ling Fung Chan <justinlfchan@gmail.com>
2022-09-24 18:28:08 +0100

    completed dog_years

---
+++ dog_years.js
@@ -0,0 +1,19 @@
+const myAge = 26;
+
+let earlyYears = 2;
+earlyYears = 2 * 10.5;
+
+let laterYears = myAge - 2;
+laterYears = laterYears * 4;
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+const myName = "Justin Chan";
+myName.toLowerCase;
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+
+
+console.log(earlyYears);
+console.log(laterYears);
